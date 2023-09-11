57969f65fc4abfe58125d4ce5c5b86d554ff7692
shahab-edraak <shahab@edraaksystems.com>
2023-09-11 14:37:00 +0500

    initial

---
+++ src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+const Home = () => {
+    return (
+        <div>
+            <div><Navbar /></div>
+            <h2>Home</h2>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
