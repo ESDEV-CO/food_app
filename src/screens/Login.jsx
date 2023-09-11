7ebcad0b521db2cacd7f3d80008b0bf758747bf1
shahab-edraak <shahab@edraaksystems.com>
2023-09-11 16:11:49 +0500

    card,carousel,footer,navbar added

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Card from '../components/Card'
-import Carousel from '../components/Carousel'
 
-
-const Home = () => {
+const Login = () => {
     return (
         <div>
             <Navbar />
-            <Carousel />
-            <Card />
+            <h1>Login</h1>
             <Footer />
         </div>
     )
 }
 
-export default Home
+export default Login
