e4a6b1731e4cbe1f5371df9c14293b5d0e599dc6
shahab-edraak <shahab@edraaksystems.com>
2023-09-12 18:33:09 +0500

    user schema + user creating

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const port = 5000
 const mongoDB = require('./db')
 mongoDB()
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
+const foodItemSchema = new mongoose.Schema({
+    CategoryName: String,
+    name: String,
+    img: String,
+    options: {
+        type: Array
+    },
+    description: String,
+});
+const FoodItem = mongoose.model('food_items', foodItemSchema);
+app.use(express.json())
+app.use('/api', require("./Routes/CreateUser"))
+
+app.get('/', async (req, res) => {
+    try {
+        const data = await FoodItem.find()
+        // console.log('Data:', data);
+        res.json(data);
+    } catch (err) {
+        // console.error('Error:', err);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
