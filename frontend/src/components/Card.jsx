476fe484927385ac23be6c798cea151c9bc406f4
shahab-edraak <shahab@edraaksystems.com>
2023-09-14 15:59:04 +0500

    home page and search functionality added

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+    let options = props.options;
+    let priceOptions = Object.keys(options)
+
     return (
         <div className="card mt-3 " style={{ "width": " 18rem", objectFit: 'contain !important' }}>
-            <img src="https://source.unsplash.com/random/300x300/?food-plate" className="card-img-top" alt="..." />
+            <img src={props.imgSrc} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some Important text </p>
+                <h5 className="card-title">{props.foodName}</h5>
+                <p className="card-text">{props.description}</p>
                 <div className="container w-100 d-flex justify-content-center align-items-center gap-2  ">
                     <select className='m-2 h-100  bg-success text-white rounded  '>
                         {Array.from(Array(6), (e, i) => {
@@ -16,8 +19,9 @@ const Card = () => {
                         })}
                     </select>
                     <select className=' h-100  bg-success text-white rounded  '>
-                        <option value="half">Half</option>
-                        <option value="full">Full</option>
+                        {priceOptions.map((data) => {
+                            return <option key={data} value={data}>{data}</option>
+                        })}
                     </select>
                     <div className='h-100 fs-5 '>Total Price</div>
                 </div>
