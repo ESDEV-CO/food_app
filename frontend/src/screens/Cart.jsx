b4239734aedbb18ae3cf401a2e2dd28b9c7a3313
shahab-edraak <shahab@edraaksystems.com>
2023-09-21 16:30:45 +0500

    payment integration in cart not in checkout page

--- frontend/src/screens/Cart.jsx
+++ frontend/src/screens/Cart.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCart, useDispatchCart } from '../components/ContextReducer';
 import { AiOutlineDelete } from 'react-icons/ai'
-import { useNavigate } from 'react-router';
+// import { useNavigate } from 'react-router';
+import { loadStripe } from '@stripe/stripe-js'
+
 
 
 const Cart = () => {
+    const [isLoading, setIsLoading] = useState(false);
     let data = useCart();
     let dispatch = useDispatchCart();
-    let navigate = useNavigate()
+    // let navigate = useNavigate()
 
     if (data.length === 0) {
         return (
@@ -16,7 +19,41 @@ const Cart = () => {
             </div>
         )
     }
+    const makePayment = async () => {
+        const stripe = await loadStripe("pk_test_51NsK7LI97mld8Ot2sexNH1X7Db8ow2clm9UyAw1VDTUO7VLNhxas5EKkSQiP8WGJpKJYm9QZsoiIDYeK9TDmSK1O00C6nutpu4");
+
+        const response = await fetch('http://localhost:5000/api/checkout-session', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                totalPricePay: totalPrice
+            })
+        });
+
+        if (response.status === 200) {
+            const session = await response.json();
+
+            // console.log("session ::", session)
+
+
+            // Now you can access the session data
+            const result = await stripe.redirectToCheckout({
+                sessionId: session.id,
+            });
+
+            if (result.error) {
+                console.log(result.error);
+            }
+        } else {
+            // Handle non-200 status codes, e.g., display an error message
+            console.error('API request failed with status:', response.status);
+        }
+
+    };
     const handleCheckOut = async () => {
+        setIsLoading(true);
         let userEmail = localStorage.getItem("userEmail");
         // console.log(data,localStorage.getItem("userEmail"),new Date())
         let response = await fetch("http://localhost:5000/api/orderData", {
@@ -36,8 +73,12 @@ const Cart = () => {
         // console.log("JSON RESPONSE:::::", response.status)
         // console.log("JSON  Data:::::", response)
         if (response.status === 200) {
+
+            await makePayment()
+
             dispatch({ type: "DROP" })
-            navigate('/checkout')
+            // navigate('/checkout')
+            setIsLoading(false);
         }
     }
 
@@ -73,7 +114,7 @@ const Cart = () => {
                 </table>
                 <div className='d-flex justify-content-between'>
                     <h3>Total Price: {totalPrice}</h3>
-                    <button className="btn bg-success text-white mt-1" onClick={handleCheckOut}>Check Out</button>
+                    <button className="btn bg-success text-white mt-1" onClick={handleCheckOut}>{isLoading ? 'Loading...' : 'Check Out'}</button>
                 </div>
             </div>
         </div>
